40c2187277abf0d5913a3f0e3febbc866e969fda
juansedev2 <jsab62tol@gmail.com>
2023-08-22 16:43:01 -0500

    Agregado la vista de Landing Page en archivo view.php

--- public/views/Landing.view.php
+++ public/views/Landing.view.php
@@ -1,3 +1,31 @@
-<?php
-
-require_once "./public/html/Landing.html";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="juansedev2">
+    <title>Document</title>
+    <link rel="stylesheet" href="/public/css/Normalize.css">
+    <link rel="stylesheet" href="/public/css/Landing.css">
+</head>
+<body>
+    <nav id="navigate_section">
+        <ul class="navigate_menu">
+            <li><a href="login">Iniciar sesión</a></li>
+            <li><a href="registro">Registrarse</a></li>
+            <li><a href="acerca">Acerca de</a></li>
+        </ul>
+    </nav>
+    <section id="welcome_section">
+        <div id="welcome_box">
+            <div class="welcome_message">
+                <h1>Bienvenido</h1>
+                <img src="/public/assets/img/UserIcon.png" alt="Imagen no encontrada" width="200px">
+            </div>
+            <div>
+                <h2>Sistema de administración de usuarios</h2>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
