8ae5cc9eb4761337e3290f41b5d18ddfa9ca69a5
juansedev2 <jsab62tol@gmail.com>
2023-08-23 16:01:22 -0500

    Agregado la vista de acerca de

---
+++ public/views/About.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="juansedev2">
+    <title>Registro</title>
+    <link rel="stylesheet" href="/public/css/Normalize.css">
+    <link rel="stylesheet" href="/public/css/NavMenu.css">
+    <link rel="stylesheet" href="/public/css/About.css">
+</head>
+<body>
+    <?php require "./public/partials/NavMenu.html" ?>
+    <section id="about_section">
+        <h1>Acerca del sistema de administración de usuarios</h1>
+        <p>
+            El sistema de administración de personas administra sus usuarios de acuerdo
+            a la organización que la utilice, nuestro servicio es proveer una plataforma que ofrezca
+            la facilidad de administrar los usuarios de una organización y sus registros, la cual pueden
+            adoptar a las necesidades de la misma, bien sea solicitando agregar más funcionalidades o bien
+            usarlo en su nivel más básico para controlar los registros de sus colaboradores. Para más información
+            acerca de como adquirir nuestro producto, soporte, PQRS o demás, favor comunicarse con nosotros.
+        </p>
+    </section>
+</body>
+</html>
