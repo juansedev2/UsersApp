8c4900f0fcecc11f5887aa15a67a61b15ce64ec0
juansedev2 <jsab62tol@gmail.com>
2023-08-31 18:19:00 -0500

    Finalizado la estructura visual del perfil del usuario

--- public/views/AdministratorProfile.view.php
+++ public/views/AdministratorProfile.view.php
@@ -6,11 +6,12 @@
     <title>Perfil</title>
     <link rel="stylesheet" href="/public/css/Normalize.css">
     <link rel="stylesheet" href="/public/css/ProfileData.css">
+    <link rel="stylesheet" href="/public/css/NavProfileUser.css">
     <?php include_once "./public/partials/Favicon.html"?>
 </head>
 <body>
     <?php include_once "./public/partials/NavProfileUser.html"?>
-    <section>
+    <section id="section_profile_tittle">
         <h1>Administrador/a: <?=$_SESSION["name"]?></h1>
     </section>
     <section id="section_profile_data">
