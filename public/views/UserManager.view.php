0402794aac5615f2d66d540d8588cc771b85878a
juansedev2 <jsab62tol@gmail.com>
2023-09-05 17:05:46 -0500

    Finalizado la interfaz de usuarios en cuanto a su aspecto visual

--- public/views/UserManager.view.php
+++ public/views/UserManager.view.php
@@ -7,6 +7,7 @@
     <?php include_once "./public/partials/Favicon.html"?>
     <link rel="stylesheet" href="/public/css/Normalize.css">
     <link rel="stylesheet" href="/public/css/NavMenu.css">
+    <link rel="stylesheet" href="/public/css/UserManager.css">
 </head>
 <body>
     <?php require_once "./public/partials/NavProfileUser.html"?>
@@ -14,23 +15,27 @@
         <h1>Administración de usuarios</h1>
     </section>
     <main id="users_table">
-        <table border="1">
-            <tr>
-                <th>Id usuario</th>
-                <th>Nombre completo</th>
-                <th>Ver usuario</th>
-                <th>Editar usuario</th>
-                <th>Eliminar usuario</th>
-            </tr>
-            <?php foreach ($users as $user):?>
+        <table>
+            <thead>
                 <tr>
-                    <td><?=$user->properties["id"]?></td>
-                    <td><?=$user->properties["first_name"]. " " . $user->properties["middle_name"] . " " . $user->properties["last_name"]?></td>
-                    <td><a href=""><img src="/public/assets/icons/eye.ico" alt="Icono de visualizar no encontrado"></a></td>
-                    <td><a href=""><img src="/public/assets/icons/pencil.ico" alt="Icono de lapiz no encontrado"></a></td>
-                    <td><a href=""><img src="/public/assets/icons/trash.ico" alt="Icono de caneca no encontrado"></a></td>
+                    <th>Id usuario</th>
+                    <th>Nombre completo</th>
+                    <th>Ver usuario</th>
+                    <th>Editar usuario</th>
+                    <th>Eliminar usuario</th>
                 </tr>
-            <?php endforeach?>
+            </thead>
+            <tbody>
+                <?php foreach ($users as $user):?>
+                    <tr>
+                        <td><?=$user->properties["id"]?></td>
+                        <td><?=$user->properties["first_name"]. " " . $user->properties["middle_name"] . " " . $user->properties["last_name"]?></td>
+                        <td><a href=""><img src="/public/assets/icons/eye.ico" alt="Icono de visualizar no encontrado" class="icon"></a></td>
+                        <td><a href=""><img src="/public/assets/icons/pencil.ico" alt="Icono de lapiz no encontrado" class="icon"></a></td>
+                        <td><a href=""><img src="/public/assets/icons/trash.ico" alt="Icono de caneca no encontrado" class="icon trash"></a></td>
+                    </tr>
+                <?php endforeach?>
+            </tbody>
         </table>
     </main>
 </body>
